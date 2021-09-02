bbfa028ca093866d96725dbfbbbd5e1438c8b1e8
Samuel McKnight <samuellp@gmail.comgit statusgit config --global user.user.email iamsamuellp@gmail.com>
2021-09-01 21:47:58 -0500

    fixed up work employess index to work with customers with same zipcode also added customers navbar.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -16,7 +16,20 @@ def index(request):
     Customer = apps.get_model('customers.Customer')
     all_customers = Customer.objects.all
     user =request.user
-    
+    logged_in_employee = Employee.objects.get(user=user)
+    Customers = apps.get_model('customers.Customer')
+    all_customers = Customers.objects.all()
+    two_date = date.today()
+    this_day = str(date.today())
+    weekday = request.POST.get('day_of_the_week')
+    todays_customers = []
+    for customer in all_customers: 
+        customer_suspend_start = str(customer.suspend_start)
+        customer_suspend_end = str(customer.suspend_end)
+        if  this_day < customer_suspend_start or this_day > customer_suspend_end:     
+            if customer.zip_code == logged_in_employee.zip_code and (customer.weekly_pickup == weekday or customer.one_time_pickup == weekday) :
+                todays_customers.append(customer)
+
     try: 
         logged_in_employee = Employee.objects.get(user=user)
         
@@ -24,10 +37,13 @@ def index(request):
     except:
         return HttpResponseRedirect(reverse('employees:create'))
 
-    context = {'logged_in_employee': logged_in_employee,
-                'all_customers':all_customers
-        }
+    context = { 'todays_customers' : todays_customers,
+                'weekday': weekday}
     return render(request,'employees/index.html', context)
+
+
+ 
+    
 
 def create(request):
     if request.method == 'POST':
